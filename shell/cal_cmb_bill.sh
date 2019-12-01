42ee64444bec3cc00e30e0c4dc8a3755e44eccc5
Qingsu Chen <cqs.pub@gmail.com>
2019-12-01 21:40:34 +0800

    更新统计招行信用卡账单的脚本

--- shell/cal_cmb_bill.sh
+++ shell/cal_cmb_bill.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
-BEGIN_DATE="2018-03-08"	# 记账日为准
-END_DATE="2018-04-07"   # 记账日为准
+BEGIN_DATE="2019-11-07"	# 交易日为准
+END_DATE="2019-12-01"   # 交易日为准
+
+# 输入的csv格式要求：
+# 第一列为：2019-11-01 这样日期格式
+# 编码为UTF-8,换行为unix
+# 获取步骤：
+#	1. 招行大众银行网页版下载未出账单的userdata.csv文件
+#	2. sed -i 's/\t//g' userdata.csv 去掉多余的制表符
+#	3. excel打开该文件, 调整第一列日期格式为2019-11-01，然后另存为制表符分隔的txt文件: userdata.txt
+#	4. dos2unix userdata.txt; iconf -f gbk -t utf8 < userdata.txt > userdata.csv
 
 # sed returns self-contained awk script, [] to prevent sed line
-awk -v begin=$BEGIN_DATE -v end=$END_DATE -f <(sed -e '/[B]EGIN_AWK1/,/[E]ND_AWK1/!d' $0) <(cat "$@" | iconv -f gbk -t utf8 )
+awk -v begin=$BEGIN_DATE -v end=$END_DATE -f <(sed -e '/[B]EGIN_AWK1/,/[E]ND_AWK1/!d' $0) "$@"
 
 exit 
 # shell script end here
@@ -13,43 +22,32 @@ exit
 
 BEGIN {
 	FS="\t"
-	OFS="\t"
+	OFS=","
 }
 
 # 跳过列头
-$0 !~ /对账标志/ {
-	sub(",", "", $4)	# date
-	gsub(",", "", $5)	# details
-	split($5, a, "\"")
-	date = $4
+NR > 1 {
+	date = $1
 	if (date >= begin && date <= end) {
-	        payee = a[1]
-		money = a[4]
-		day = (substr(date, 9, 2)+0)	# convert to numberic
+	        payee = $3
+		gsub("\042", "", $6)
+		gsub(",", "", $6)
+		money = substr($6,2)
 
-		# 还的是上期账单
-		#if (payee == "掌上生活还款" && day >= 8 && day <= 25) {
-		#	next
-		#}
-		if(money > 0) {
-			sum[date] += money
-			total += money
-		}
-		if(money <0 ) {
-			payback[date] += money
-			payback_total += money
-		}
+		if (payee ~ /还款/ )	# skip payback
+			next
+
+		cnt[date] += 1
+		sum[date] += money
+		total += money
 	}
 }
 
 END { 
 	for (date in sum) 
-		printf("%s\t%.2f\n", date, sum[date])
-	
-	printf("消费合计: %.2f\n", total)
-	for (date in payback)
-		printf("%s\t%.2f\n", date, payback[date])
-	printf("还款合计：%.2f\n", payback_total)
+		printf("%s,%d,%.2f\n", date, cnt[date], sum[date])
+
+	printf("total,%.2f\n", total)
 }
 
 #END_AWK1
